a5e6db59e53c33f81636f8dd28ee6092f8130c58
Arianna Rassu <arianna.rassu82@gmail.com>
2024-11-23 20:18:40 +0100

    Cuatro ejercicios

--- evaluacion_mod2.sql
+++ evaluacion_mod2.sql
@@ -7,6 +7,8 @@ SELECT
 DISTINCT f.title AS titulos_peliculas
 FROM film AS f;
 
+
+
 -- 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".
 
 SELECT
@@ -14,3 +16,25 @@ DISTINCT f.title AS titulos_peliculas,
 f.rating AS clasificación
 FROM film AS f
 WHERE f.rating = 'PG-13';
+
+
+
+-- 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
+
+SELECT
+DISTINCT f.title AS titulos_peliculas,
+f.description 
+FROM film AS f
+WHERE f.description LIKE "%AMAZING%";
+
+
+
+-- 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
+
+SELECT
+DISTINCT f.title AS titulos_peliculas,
+f.length
+FROM film AS f
+WHERE f.length > 120;
+
+
